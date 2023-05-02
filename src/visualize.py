7d84453b571558f0514f54a42c593a65cbffb4fd
Pedro Arellano <PArellano25@cmc.edu>
2023-05-01 20:03:17 -0700

    first

--- src/visualize.py
+++ src/visualize.py
@@ -10,13 +10,17 @@ args = parser.parse_args()
 
 # imports
 import os
+import matplotlib
+matplotlib.use('Agg') 
+import matplotlib.pyplot as plt
 import json
 from collections import Counter,defaultdict
 
 # open the input path
 with open(args.input_path) as f:
     counts = json.load(f)
-
+#    print("counts=", counts)
+    
 # normalize the counts by the total values
 if args.percent:
     for k in counts[args.key]:
@@ -26,3 +30,25 @@ if args.percent:
 items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
 for k,v in items:
     print(k,':',v)
+
+keys = []
+values = []
+for k,v in items[:10]:
+    keys = keys + [k]
+    values = values + [v]
+
+plt.bar(k, v)
+
+if 'lang' in str(args.input_path):
+    plt.title('Spread of Coronavirus in Through Language in Twitter')
+    plt.xlabel('languange')
+    plt.ylabel('count')
+    plt.legend()
+    plt.savefig(f'{args.key} + _country.png')
+
+if "country" in str(args.input_path):
+    plt.title('Spread of Coronavirus in countries in Twitter')
+    plt.xlabel('countries')
+    plt.ylabel('count')
+    plt.legend()
+    plt.savefig(f'{args.key} + _country.png')
